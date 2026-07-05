2d5b5a9d983fbcbc12d739a6965e2e0564378e00
aenuralife <aenurallif@gmail.com>
2026-07-05 14:39:38 +0700

    Support Render deployment

--- server.js
+++ server.js
@@ -190,10 +190,11 @@ app.get("/api/discover", async (req, res) => {
 
 });
 tfidfEngine.initialize();
-app.listen(3000, () => {
 
-    console.log(
-        "Server running at http://localhost:3000"
-    );
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
 
 });
